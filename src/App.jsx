332857145abc7eb23a9dd130d0ecefb581b2ae90
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-09 16:00:46 -0300

    Agregue un fetch de la api de giphy

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import './App.css'
 function App() {
 
   const [ValorInput, setValorInput] = useState("");
+  const [gifs, setGifs] = useState();
 
   const handleChange = (event) => {
     const valor = event.target.value;
     setValorInput(valor);
     console.log(event.target.value);
   }
+
+  const getGifs = async () => {
+    const url = "https://api.giphy.com/v1/gifs/search?api_key=2xxlV44o6d1jbv1JQcJ265qb0jBNE22E&q=cheeseburgers";
+
+      const response = await fetch(url);
+      if(!response.ok){
+        console.log("Error");
+      }
+
+      const data = await response.json();
+      console.log(data)
+  }
+
+  getGifs();
 
   return (
     <>
