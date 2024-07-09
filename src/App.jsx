4dc38bed253917c3a24f0b2dbe1f0ff007274bde
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-09 15:25:52 -0300

    termine el input basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ValorInput, setValorInput] = useState("");
+
+  const handleChange = (event) => {
+    const valor = event.target.value;
+    setValorInput(valor);
+    console.log(event.target.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value={ValorInput} onChange={(event) => handleChange(event)}/>
+
+      <p>{ValorInput}</p>
     </>
   )
 }
